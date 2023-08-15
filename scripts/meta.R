af2abf4ccf92b12a244d44cc7467df811b35e5ef
Daphne Weemering <dnweemering@gmail.com>
2023-08-15 10:15:40 +0200

    update meta-analyses

--- scripts/meta.R
+++ scripts/meta.R
@@ -5,7 +5,7 @@ library(metafor)
 library(tidyverse)
 library(dplyr)
 
-dat <- read_excel('meta.xlsx')
+dat <- read_excel('meta2.xlsx')
 
 fclttrs <- list('Assessment burden', 'Awareness', 'Clinic reputation', 
                 'Disruption in current medication', 'Financial compensation',
@@ -15,8 +15,8 @@ fclttrs <- list('Assessment burden', 'Awareness', 'Clinic reputation',
 
 datalist <- list()
 for (i in 1:length(fclttrs)){
-  datalist[[i]] <- subset(dat, dat$Facilitator == fclttrs[[i]])
-  datalist[[i]]$mod <- ifelse(datalist[[i]]$Disease != 'AD', 0, 1)
+  datalist[[i]] <- subset(dat, dat$FACILITATOR == fclttrs[[i]])
+  datalist[[i]]$mod <- ifelse(datalist[[i]]$DISEASE != 'AD', 0, 1)
 }
 
 
@@ -29,7 +29,7 @@ colnames(store) <- c('TE', 'LB', 'UB', 'k', 'events', 'total', 'facilitator', 'I
                      'nAD_LB', 'nAD_UB', 'OR_LB', 'OR_UB', 'AD_LB', 'AD_UB')
 
 for(i in 1:length(datalist)){
-  m <- rma.glmm(xi = n_respond, ni = n_total, data = datalist[[i]], measure = 'PLO')
+  m <- rma.glmm(xi = N, ni = TOTAL, data = datalist[[i]], measure = 'PLO')
   if(length(datalist[[i]]$mod) > 2){
     mmod <- update(m, mods = mod)
     predicted <- stats::predict(mmod, newdata = data.frame(mods = c(0,1)))
@@ -41,10 +41,10 @@ for(i in 1:length(datalist)){
   store[i, 2] <- mb$ci.lb*100
   store[i, 3] <- mb$ci.ub*100
   store[i, 4] <- m$k
-  store[i, 5] <- sum(datalist[[i]]$n_respond)
+  store[i, 5] <- sum(datalist[[i]]$N)
   store[i, 6] <- sum(m$ni)
-  store[i, 7] <- datalist[[i]]$Facilitator[[1]]
-  datalist[[i]]$sep_prop <- (datalist[[i]]$n_respond/datalist[[i]]$n_total)*100
+  store[i, 7] <- datalist[[i]]$FACILITATOR[[1]]
+  datalist[[i]]$sep_prop <- (datalist[[i]]$N/datalist[[i]]$TOTAL)*100
   
   if(length(datalist[[i]]$mod) > 2){
     # heterogeneity statistics
@@ -55,16 +55,16 @@ for(i in 1:length(datalist)){
     store[i, 12] <- m$QEp.LRT
     
     # estimates with moderator
-  store[i, 13] <- exp(mmod$beta[1])/(1 + exp(mmod$beta[1]))*100
-  store[i, 14] <- exp(mmod$beta[1] + mmod$beta[2])/(1 + exp(mmod$beta[1] + mmod$beta[2]))*100
-  store[i, 15] <- exp(mmod$beta[2])
-  store[i, 16] <- mmod$pval[2]
-  store[i, 17] <- plogis(mmod$ci.lb[[1]])*100
-  store[i, 18] <- plogis(mmod$ci.ub[[1]])*100
-  store[i, 19] <- exp(mmod$ci.lb[[2]])
-  store[i, 20] <- exp(mmod$ci.ub[[2]])
-  store[i, 21] <- plogis(predicted$ci.lb[[1]])*100
-  store[i, 22] <- plogis(predicted$ci.ub[[1]])*100
+    store[i, 13] <- exp(mmod$beta[1])/(1 + exp(mmod$beta[1]))*100
+    store[i, 14] <- exp(mmod$beta[1] + mmod$beta[2])/(1 + exp(mmod$beta[1] + mmod$beta[2]))*100
+    store[i, 15] <- exp(mmod$beta[2])
+    store[i, 16] <- mmod$pval[2]
+    store[i, 17] <- plogis(mmod$ci.lb[[1]])*100
+    store[i, 18] <- plogis(mmod$ci.ub[[1]])*100
+    store[i, 19] <- exp(mmod$ci.lb[[2]])
+    store[i, 20] <- exp(mmod$ci.ub[[2]])
+    store[i, 21] <- plogis(predicted$ci.lb[[1]])*100
+    store[i, 22] <- plogis(predicted$ci.ub[[1]])*100
   }
   
 }
@@ -73,7 +73,7 @@ datalist <- datalist[order(store[, 'TE'])]
 store <- store[order(store[, 'TE'], decreasing = F),]
 
 #### FIGURE ####
-pdf(file = '/Users/daphneweemering/surfdrive/trial participation/trial-participation/figures/figure3.pdf', 
+pdf(file = '/Users/daphneweemering/surfdrive/trial participation/trial-participation/figures/test.pdf', 
     width = 11.7, height = 7)
 
 par(mar = c(4.5, 34, 3, 1), tck = -0.01, xpd = T)
@@ -90,7 +90,7 @@ for(i in 1:length(datalist)){
 
 # estimates
 for (i in 1:length(datalist)){
-  text(x = -153, y = i+0.1, datalist[[i]]$Facilitator[1], cex = 1, adj = c(0,0.5))
+  text(x = -153, y = i+0.1, datalist[[i]]$FACILITATOR[1], cex = 1, adj = c(0,0.5))
   text(x = -85, y = i+0.1, store[i,'k'], cex = 1)
   text(x = -60, y = i+0.1, paste0(store[i, 'events'], '/', store[i, 'total']), cex = 1)
   text(x = -25, y = i+0.1, paste0(format(round(as.numeric(store[i, 'TE']), 2), nsmall = 2), ' (',
